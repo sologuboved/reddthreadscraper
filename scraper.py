65381e87d9e230da060b22b4ab6dcc38647d23e1
sologuboved <sologuboved@gmail.com>
2023-05-26 01:58:49 +0300

    WIP

--- scraper.py
+++ scraper.py
@@ -1,19 +1,18 @@
 import praw
 
-from helpers import delete_pid, dump_utf_json, which_watch, write_pid
+from helpers import delete_pid, dump_utf_json, get_abs_path, which_watch, write_pid
 from userinfo import R_CLIENT_ID, R_CLIENT_SECRET, R_PASSWORD, R_USER_AGENT, R_USERNAME
 
 
-class Batcher:
-    def __init__(self, raw_thread, batch_size):
-        self.raw_thread = raw_thread
-
-
 class Scraper:
-    def __init__(self, url, by_old, batch_size):
+    def __init__(self, url, by_old, batch_size, txt):
         self.url = url
         self.by_old = by_old
         self.batch_size = batch_size
+        if txt:
+            self.writer = self.write_txt
+        else:
+            self.writer = self.write_json
         self.filename = self.get_filename()
 
     def get_filename(self):
@@ -23,7 +22,7 @@ class Scraper:
             url = self.url
         return url.rsplit('/', 1)[-1]
 
-    def get_thread(self):
+    def get_submission(self):
         reddit = praw.Reddit(
             client_id=R_CLIENT_ID,
             client_secret=R_CLIENT_SECRET,
@@ -40,6 +39,10 @@ class Scraper:
         submission.comments.replace_more(limit=None)
         print("...Replaced more")
 
+        return submission
+
+    @staticmethod
+    def get_comments(submission):
         for comment in submission.comments.list():
             text = comment.body
             if text == '[deleted]':
@@ -55,8 +58,47 @@ class Scraper:
                 'text': text,
             }
 
+    def get_batch(self, raw_tread):
+        batch = list()
+        count = index = 0
+        for comment in raw_tread:
+            batch.append(comment)
+            count += 1
+            if count == self.batch_size:
+                yield index, batch
+                batch = list()
+                count = 0
+                index += 1
+        yield index, batch
+
+    @staticmethod
+    def write_txt(thread, filename):
+        filename += '.txt'
+        return filename
+
+    @staticmethod
+    def write_json(thread, filename):
+        filename += '.json'
+        dump_utf_json(thread, filename)
+        return filename
+
+    @which_watch
+    def main(self):
+        submission = self.get_submission()
+        num_comments = submission.num_comments
+        raw_thread = self.get_comments(submission)
+        if self.batch_size:
+            filenames = list()
+            if num_comments > self.batch_size + 9:
+                zfill = len(str(num_comments // self.batch_size + 1))
+                for index, batch in self.get_batch(raw_thread):
+                    filename = self.writer(batch, f'{self.filename}_{str(index).zfill(zfill)}')
+                    filenames.append(filename)
+                return filenames
+        return self.writer(list(raw_thread), get_abs_path(self.filename))
+
 
 if __name__ == '__main__':
     pid_fname = write_pid()
-    ('https://www.reddit.com/r/AskReddit/comments/ucaltb/what_are_some_simple_yet_incredibly/', False, False)
+    # ('https://www.reddit.com/r/AskReddit/comments/ucaltb/what_are_some_simple_yet_incredibly/', False, False)
     delete_pid(pid_fname)
