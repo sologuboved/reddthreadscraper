71bc01a4c8b51199bd4c7d8a6d030ac875e30d63
sologuboved <sologuboved@gmail.com>
2023-05-26 23:54:51 +0300

    WIP

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import asyncio
 from functools import wraps
 import json
 import logging
@@ -55,18 +56,16 @@ def check_auth(func):
 
 def report_exception(func):
     @wraps(func)
-    async def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except Exception as e:
             notification = f"({func.__name__}, called with {args}, {kwargs}) {type(e).__name__}: {e}"
-            notifier = Bot(token=T_TOKEN)
-            async with notifier:
-                await notifier.send_message(
-                    chat_id=T_REDDTHREADSCRAPER_ID,
-                    text=notification,
-                    disable_web_page_preview=True,
-                )
+            asyncio.run(Bot(token=T_TOKEN).send_message(
+                chat_id=T_REDDTHREADSCRAPER_ID,
+                text=notification,
+                disable_web_page_preview=True,
+            ))
             traceback_msg = traceback.format_exc()
             logging.error(traceback_msg)
 
